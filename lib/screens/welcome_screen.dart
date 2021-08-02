21e328e55c95129902829dd612b0db3bc2ac4451
nvmb3rth30ry <nvmb3rth30ry@gmail.com>
2021-08-02 18:46:50 +1000

    Animations etc

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController aniController;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    int n = 0;
+
+    aniController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+      upperBound: 1,
+    );
+
+    // animation = CurvedAnimation(
+    //   parent: aniController,
+    //   curve: Curves.easeInQuart,
+    // );
+
+    aniController.forward();
+    aniController.addListener(() {
+      setState(() {});
+      n++;
+      print('iter #${n.toString()} : ${animation.value.toString()}');
+    });
+
+    // // // Endless ping=pong animation loop // // //
+    // aniController.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     aniController.reverse(from: 1);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     aniController.forward();
+    //   }
+    // });
+  }
+
+  // Ensure animation control object is destroyed when no longer used
+  @override
+  void dispose() {
+    aniController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: animation.value * 100.0, //60.0,
+                  ),
+                ),
+                Text(
+                  'Flash Chat',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Log In',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Register',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
